5f84cbaaa27314b312773d1446ed9508b6c3b1cb
flyalive <nanireu@gmail.com>
2022-05-07 00:09:04 +0900

    익명함수에 name을 줘서 코드를 간략하게 변경함
    익명함수에 name을 줘서 코드를 간략하게 변경함
    함수명 lottoNum

--- lotto.js
+++ lotto.js
@@ -1,26 +1,26 @@
 'use strict';
+function lottoNum () {
+    let numArr = [];
 
-let numArr = [];
+    for (let i=0; i<6; i++) {
+        let num = Math.floor(Math.random() * 45 + 1 );
 
-for (let i=0; i<6; i++) {
-    let num = Math.floor(Math.random() * 45 + 1 );
+        for (let j in numArr) {
+            if (num === numArr[j]){
+            num = Math.floor(Math.random() * 45 )+ 1;
+            }
+        };
 
-    for (let j in numArr) {
-        if (num === numArr[j]){
-        num = Math.floor(Math.random() * 45 )+ 1;
-        }
-    };
+    numArr.push(num)
+    }
 
-numArr.push(num)
+    // 배열을 오름차순으로 정리하기,
+    numArr.sort(function (a,b) {
+    return a-b;
+    })
+    return numArr;
+
 }
+lottoNum();
 
-numArr.sort(function (a,b) {
-return a-b;
-})
-
-document.write(`이번주의 메인 로또 추천번호는 : `, numArr);
-
-
-
-
-
+document.write(`이번주의 메인 로또 추천번호는 : `, lottoNum());
